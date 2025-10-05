aba814f07129cb45b90c4e2f68a3aa1377afb65c
rrandel-tech <robert.randel46@gmail.com>
2025-10-05 22:25:57 +0000

    Build: (bfbe4df) Update publish.yml

--- README.md
+++
@@ -1,5 +0,0 @@
-# Luma Engine Website
-
-Official website for Luma - a high-performance 3D game engine for desktop platforms.
-
-
--- src/components/index.jsx
+++
@@ -1,9 +0,0 @@
-// Layout components
-export {default as Navbar} from './layout/Navbar/Navbar.jsx';
-export {default as Footer} from './layout/Footer';
-
-// UI components
-export {default as CategoryFilter} from './ui/CategoryFilter';
-export {default as DownloadButton} from './ui/DownloadButton';
-export {default as ThemeButton} from './ui/ThemeButton';
-export {default as Pagination} from './ui/Pagination';
