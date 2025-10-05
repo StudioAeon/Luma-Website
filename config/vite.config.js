2214ae9ac7c7aa1b746dd1a43304122d8ce2453c
rrandel-tech <robert.randel46@gmail.com>
2025-10-05 18:20:42 -0400

    Create publish.yml

--- config/vite.config.js
+++ config/vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
       react(),
-      tailwindcss()
+      tailwindcss(),
+      basicSsl()
   ],
+  server: {
+    https: true,
+    host: 'localhost',
+    port: 3000,
+    strictPort: true,
+    open: true
+  }
 })
